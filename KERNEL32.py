3074b9b32c9ab7d603ac7416de2eb095c817b607
SkyLined <github@skylined.nl>
2017-10-09 15:17:27 +0200

    Add more KERNEL32 functions

--- KERNEL32.py
+++ KERNEL32.py
@@ -3,13 +3,24 @@ from Types import *;
 
 KERNEL32 = cDLL("kernel32.dll");
 
+KERNEL32.fDefineFunction(BOOL,    "CloseHandle", HANDLE);
+KERNEL32.fDefineFunction(BOOL,    "GenerateConsoleCtrlEvent", DWORD, DWORD);
 KERNEL32.fDefineFunction(BOOL,    "GetConsoleMode", HANDLE, PDWORD);
 KERNEL32.fDefineFunction(BOOL,    "GetConsoleScreenBufferInfo", HANDLE, PCONSOLE_SCREEN_BUFFER_INFO);
 KERNEL32.fDefineFunction(DWORD,   "GetLastError");
+KERNEL32.fDefineFunction(HANDLE,  "GetProcessHeap");
 KERNEL32.fDefineFunction(DWORD,   "GetShortPathNameA", LPCSTR, LPSTR, DWORD);
 KERNEL32.fDefineFunction(DWORD,   "GetShortPathNameW", LPCWSTR, LPWSTR, DWORD);
 KERNEL32.fDefineFunction(HANDLE,  "GetStdHandle", DWORD);
+KERNEL32.fDefineFunction(LPVOID,  "HeapAlloc", HANDLE, DWORD, SIZE_T);
+KERNEL32.fDefineFunction(HANDLE,  "HeapCreate", DWORD, SIZE_T, SIZE_T);
+KERNEL32.fDefineFunction(BOOL,    "HeapFree", HANDLE, DWORD, LPVOID);
+KERNEL32.fDefineFunction(LPVOID,  "HeapReAlloc", HANDLE, DWORD, LPVOID, SIZE_T);
+KERNEL32.fDefineFunction(HANDLE,  "OpenProcess", DWORD, BOOL, DWORD);
+KERNEL32.fDefineFunction(BOOL,    "OpenProcessToken", HANDLE, DWORD, PHANDLE);
 KERNEL32.fDefineFunction(BOOL,    "SetConsoleTextAttribute", HANDLE, WORD);
+KERNEL32.fDefineFunction(BOOL,    "VirtualProtect", LPVOID, SIZE_T, DWORD, PDWORD);
+KERNEL32.fDefineFunction(BOOL,    "VirtualProtectEx", HANDLE, LPVOID, SIZE_T, DWORD, PDWORD);
 KERNEL32.fDefineFunction(BOOL,    "WriteConsoleA", HANDLE, LPCSTR, DWORD, LPDWORD, LPVOID);
 KERNEL32.fDefineFunction(BOOL,    "WriteConsoleW", HANDLE, LPCWSTR, DWORD, LPDWORD, LPVOID);
 KERNEL32.fDefineFunction(BOOL,    "WriteFile", HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
