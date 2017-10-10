2ea0c66f6a5188bb9057f354dc11b5c7576a1fe5
SkyLined <github@skylined.nl>
2017-10-10 13:30:19 +0200

    Add two meta functions
    This stuff needs to be done often enough to warrant wrappers that handle
    the details

--- __init__.py
+++ __init__.py
@@ -6,4 +6,7 @@ from StructureTypes import *;
 from ADVAPI32 import ADVAPI32;
 from KERNEL32 import KERNEL32;
 
+from fbTerminateProcessForId import fbTerminateProcessForId;
+from fdsProcessesExecutableName_by_uId import fdsProcessesExecutableName_by_uId;
+
 from oVersionInformation import oVersionInformation;
