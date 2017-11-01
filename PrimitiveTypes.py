8bf554cbee556ffaf425d168db301474251179bb
SkyLined <github@skylined.nl>
2017-11-01 13:09:41 +0100

    Add more primitive types + 32/64 bit specific pointers and sizes

--- PrimitiveTypes.py
+++ PrimitiveTypes.py
@@ -16,10 +16,11 @@ BOOL = ctypes.c_long;
 BOOLEAN = ctypes.c_byte;
 BYTE = ctypes.c_byte;
 #CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-CHAR = ctypes.c_short;
+CHAR = ctypes.c_char;
 #DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 DOUBLE = ctypes.c_double;
 DWORD = ctypes.c_ulong;
+DWORD_PTR = SIZEOF(ctypes.c_void_p) == 4 and ctypes.c_ulong or ctypes.c_ulonglong;
 #FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 FLOAT = ctypes.c_float;
 #HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
@@ -55,6 +56,10 @@ HWINSTA = ctypes.c_void_p;
 HWND = ctypes.c_void_p;
 #IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 INT = ctypes.c_int;
+INT8 = ctypes.c_byte;
+INT16 = ctypes.c_short;
+INT32 = ctypes.c_int;
+INT64 = ctypes.c_longlong;
 #LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
 LONG = ctypes.c_long;
 LPCOLESTR = ctypes.c_wchar_p;
@@ -75,13 +80,23 @@ SIZE_T = ctypes.c_size_t;
 #TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 TOKEN_INFORMATION_CLASS = ctypes.c_ulong;
 #UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
-UCHAR = ctypes.c_ushort;
+UCHAR = ctypes.c_ubyte;
 UINT = ctypes.c_uint;
+UINT8 = ctypes.c_ubyte;
+UINT16 = ctypes.c_ushort;
+UINT32 = ctypes.c_uint;
+UINT64 = ctypes.c_ulonglong;
 ULONG = ctypes.c_ulong;
+ULONGLONG = ctypes.c_ulonglong;
+ULONG_PTR = SIZEOF(ctypes.c_void_p) == 4 and ctypes.c_ulong or ctypes.c_ulonglong;
 USHORT = ctypes.c_ushort;
+#VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+VOID = None
 #WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
 WCHAR = ctypes.c_wchar;
 WORD = ctypes.c_ushort;
+#QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+QWORD = ctypes.c_ulonglong;
 
 ################################################################################
 # Automatically define "P<type_name>" and "LP<type_name>" as pointer-to-type and
@@ -95,3 +110,23 @@ for sTypeName in globals().keys():
     globals()["P" + sTypeName] = POINTER(cType);
     globals()["LP" + sTypeName] = POINTER(cType);
     globals()["PP" + sTypeName] = POINTER(POINTER(cType));
+
+    globals()["P" + sTypeName + "_32"] = POINTER_32(cType);
+    globals()["LP" + sTypeName + "_32"] = POINTER_32(cType);
+    globals()["PP" + sTypeName + "_32"] = POINTER_32(POINTER_32(cType));
+    
+    globals()["P" + sTypeName + "_64"] = POINTER_64(cType);
+    globals()["LP" + sTypeName + "_64"] = POINTER_64(cType);
+    globals()["PP" + sTypeName + "_64"] = POINTER_64(POINTER_64(cType));
+
+# 32/64 bit values
+SIZE_T_32 = ctypes.c_ulong;
+SIZE_T_64 = ctypes.c_ulonglong;
+PSIZE_T_32 = POINTER_32(SIZE_T_32);
+PSIZE_T_64 = POINTER_64(SIZE_T_64);
+PPSIZE_T_32 = POINTER_32(POINTER_32(SIZE_T_32));
+PPSIZE_T_64 = POINTER_64(POINTER_64(SIZE_T_64));
+PVOID_32 = POINTER_32();
+PVOID_64 = POINTER_64();
+PPVOID_32 = POINTER_32(POINTER_32());
+PPVOID_64 = POINTER_64(POINTER_64());
