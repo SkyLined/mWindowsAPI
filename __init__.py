f9f53a9406b4f897985b998d6f33e4b2f7947528
SkyLined <github@skylined.nl>
2017-11-01 13:37:16 +0100

    Add new features to module

--- __init__.py
+++ __init__.py
@@ -6,8 +6,10 @@ from StructureTypes import *;
 from ADVAPI32 import ADVAPI32;
 from KERNEL32 import KERNEL32;
 
+from cVirtualAllocation import cVirtualAllocation;
 from fbTerminateProcessForId import fbTerminateProcessForId;
 from fdsProcessesExecutableName_by_uId import fdsProcessesExecutableName_by_uId;
+from fsGetOSISA import fsGetOSISA;
 from fuGetProcessIntegrityLevelForId import fuGetProcessIntegrityLevelForId;
 
 from oVersionInformation import oVersionInformation;
