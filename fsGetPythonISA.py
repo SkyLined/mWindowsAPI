a7d5774a7ff349345ce1e933bd8de3d599050c66
SkyLined <github@skylined.nl>
2017-11-21 12:32:31 +0100

    Cache fsGetPythonISA return value

--- fsGetPythonISA.py
+++ fsGetPythonISA.py
@@ -1,7 +1,11 @@
 import platform;
 
+gsPythonISA = None;
 def fsGetPythonISA():
-  return {
-    "32bit": "x86",
-    "64bit": "x64",
-  }[platform.architecture()[0]];
+  global gsPythonISA;
+  if gsPythonISA is None:
+    gsPythonISA = {
+      "32bit": "x86",
+      "64bit": "x64",
+    }[platform.architecture()[0]];
+  return gsPythonISA;
