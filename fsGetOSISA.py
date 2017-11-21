47cdbe1fae6db4371f7f7c1a872b7fd5b8bfbd92
SkyLined <github@skylined.nl>
2017-11-21 12:32:11 +0100

    Simplify fsGetOSISA caching

--- fsGetOSISA.py
+++ fsGetOSISA.py
@@ -1,12 +1,13 @@
 from mWindowsAPI import *;
 
-goSystemInfo = None;
+gsOSISA = None;
 
 def fsGetOSISA():
-  global goSystemInfo;
-  if goSystemInfo is None:
-    goSystemInfo = SYSTEM_INFO();
-    KERNEL32.GetNativeSystemInfo(POINTER(goSystemInfo));
-    assert goSystemInfo.wProcessorArchitecture in [PROCESSOR_ARCHITECTURE_AMD64, PROCESSOR_ARCHITECTURE_INTEL], \
-        "Unknown processor architecture %d/0x%X" % (goSystemInfo.wProcessorArchitecture, goSystemInfo.wProcessorArchitecture);
-  return goSystemInfo.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_INTEL and "x86" or "x64";
+  global gsOSISA;
+  if gsOSISA is None:
+    oSystemInfo = SYSTEM_INFO();
+    KERNEL32.GetNativeSystemInfo(POINTER(oSystemInfo));
+    assert oSystemInfo.wProcessorArchitecture in [PROCESSOR_ARCHITECTURE_AMD64, PROCESSOR_ARCHITECTURE_INTEL], \
+        "Unknown processor architecture %d/0x%X" % (oSystemInfo.wProcessorArchitecture, oSystemInfo.wProcessorArchitecture);
+    gsOSISA = oSystemInfo.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_INTEL and "x86" or "x64";
+  return gsOSISA;
