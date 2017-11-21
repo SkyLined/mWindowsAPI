07e1236b3df19592ffa16a02bcbff84d862d788b
SkyLined <github@skylined.nl>
2017-11-21 12:14:26 +0100

    Export new functions and modules

--- __init__.py
+++ __init__.py
@@ -5,13 +5,23 @@ from StructureTypes import *;
 
 from ADVAPI32 import ADVAPI32;
 from KERNEL32 import KERNEL32;
+from NTDLL import NTDLL;
 
+from cJobObject import cJobObject;
 from cVirtualAllocation import cVirtualAllocation;
+from cProcessInformation import cProcessInformation;
 from fbTerminateProcessForId import fbTerminateProcessForId;
+from fDebugBreakProcessForId import fDebugBreakProcessForId;
 from fdsProcessesExecutableName_by_uId import fdsProcessesExecutableName_by_uId;
+from foCreateVirtualAllocationInProcessForId import foCreateVirtualAllocationInProcessForId;
 from foGetRegistryValue import foGetRegistryValue;
+from fSendCtrlCToProcessForId import fSendCtrlCToProcessForId;
 from fsGetOSISA import fsGetOSISA;
+from fSuspendProcessForId import fSuspendProcessForId;
+from fbTerminateThreadForId import fbTerminateThreadForId;
+from fuCreateThreadInProcessForIdAndAddress import fuCreateThreadInProcessForIdAndAddress;
 from fuGetProcessIntegrityLevelForId import fuGetProcessIntegrityLevelForId;
+from fuGetProcessMemoryUsage import fuGetProcessMemoryUsage;
 from oWindowsVersion import oWindowsVersion;
 
 from oVersionInformation import oVersionInformation;
