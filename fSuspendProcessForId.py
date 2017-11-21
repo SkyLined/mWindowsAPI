7fb640d8625b415e66e662661a941abcecb86941
SkyLined <github@skylined.nl>
2017-11-21 12:12:51 +0100

    Add fSuspendProcessForId

---
+++ fSuspendProcessForId.py
@@ -0,0 +1,14 @@
+from mWindowsAPI import *;
+
+def fSuspendProcessForId(uProcessId):
+  hProcess = KERNEL32.OpenProcess(THREAD_SUSPEND_RESUME, FALSE, uProcessId);
+  assert hProcess, \
+      "OpenProcess(PROCESS_ALL_ACCESS, FALSE, %d/0x%X) => Error 0x%08X." % (uProcessId, uProcessId, KERNEL32.GetLastError());
+  try:
+    hResult = NTDLL.NtSuspendProcess(hProcess);
+    assert SUCCEEDED(hResult), \
+        "NtSuspendProcess(0x%08X) == %08X => Error %08X." % (hProcess, hResult, KERNEL32.GetLastError());
+    return True;
+  finally:
+    assert KERNEL32.CloseHandle(hProcess), \
+        "CloseHandle(0x%X) => Error 0x%08X" % (hProcess, KERNEL32.GetLastError());
