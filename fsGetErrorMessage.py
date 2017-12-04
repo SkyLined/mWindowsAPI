22d4b97e0874824361c0d0250ccc3c115604e55e
SkyLined <github@skylined.nl>
2017-12-04 23:18:03 +0100

    fsGetErrorMessage: Add HRESULT define name if possible

--- fsGetErrorMessage.py
+++ fsGetErrorMessage.py
@@ -1,7 +1,15 @@
 from mFunctions import HRESULT_FROM_WIN32;
 from mDLLs import KERNEL32;
+from mDefines import mErrorDefines;
 
 def fsGetErrorMessage(sFailedOperation, uError = None):
   if uError is None:
     uError = KERNEL32.GetLastError();
-  return "%s => Win32 error 0x%X / HRESULT 0x%08X." % (sFailedOperation, uError, HRESULT_FROM_WIN32(uError));
+  uHResult = HRESULT_FROM_WIN32(uError);
+  for sErrorDefineName in dir(mErrorDefines):
+    if getattr(mErrorDefines, sErrorDefineName) == uHResult:
+      sHResult = " (%s)" % sErrorDefineName;
+      break;
+  else:
+    sHResult = "";
+  return "%s => Win32 error 0x%X / HRESULT 0x%08X%s." % (sFailedOperation, uError, uHResult, sHResult);
