facc175432b4f43f57993e5ef6b706a9caa1933a
SkyLined <github@skylined.nl>
2018-05-16 11:05:26 +0200

    Code cleanup

--- fSuspendProcessForId.py
+++ fSuspendProcessForId.py
@@ -1,7 +1,7 @@
-from mDefines import FALSE, THREAD_SUSPEND_RESUME;
-from mFunctions import SUCCEEDED;
-from mDLLs import KERNEL32, NTDLL;
-from fThrowError import fThrowError;
+from .mDefines import FALSE, THREAD_SUSPEND_RESUME;
+from .mFunctions import SUCCEEDED;
+from .mDLLs import KERNEL32, NTDLL;
+from .fThrowError import fThrowError;
 
 def fSuspendProcessForId(uProcessId):
   uFlags = THREAD_SUSPEND_RESUME;
@@ -9,9 +9,9 @@ def fSuspendProcessForId(uProcessId):
   hProcess \
       or fThrowError("OpenProcess(0x%08X, FALSE, %d/0x%X)" % (uFlags, uProcessId, uProcessId,));
   try:
-    hResult = NTDLL.NtSuspendProcess(hProcess); # NOT RELIABLE!
-    SUCCEEDED(hResult) \
-        or fThrowError("NtSuspendProcess(0x%08X) == %08X" % (hProcess.value, hResult.value,));
+    uNTStatus = NTDLL.NtSuspendProcess(hProcess); # NOT RELIABLE!
+    SUCCEEDED(uNTStatus) \
+        or fThrowError("NtSuspendProcess(0x%08X)" % (hProcess.value,), uNTStatus.value);
   finally:
     KERNEL32.CloseHandle(hProcess) \
         or fThrowError("CloseHandle(0x%X)" % (hProcess.value,));
