ecca448bc126ecd9ffb87b698e48ff8a5f3dce6f
SkyLined <github@skylined.nl>
2018-05-16 11:00:36 +0200

    remove stale comment

--- fuGetProcessExitCodeForHandle.py
+++ fuGetProcessExitCodeForHandle.py
@@ -6,7 +6,6 @@ from .fbIsProcessRunningForHandle import fbIsProcessRunningForHandle;
 from .fThrowError import fThrowError;
 
 def fuGetProcessExitCodeForHandle(hProcess):
-  # We can open the process: try to terminate it.
   dwExitCode = DWORD();
   KERNEL32.GetExitCodeProcess(hProcess, POINTER(dwExitCode)) \
       or fThrowError("GetExitCodeProcess(0x%08X, ...)" % (hProcess,));
