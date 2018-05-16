8399127bfb4b612141872f035b97b44a24632280
SkyLined <github@skylined.nl>
2018-05-16 11:01:09 +0200

    Add fbWaitForThreadTerminationForHandle

---
+++ fbWaitForThreadTerminationForHandle.py
@@ -0,0 +1,19 @@
+from .mDefines import *;
+from .mFunctions import *;
+from .mTypes import *;
+from .mDLLs import KERNEL32;
+from .fThrowError import fThrowError;
+
+def fbWaitForThreadTerminationForHandle(hThread, nTimeout = None):
+  if nTimeout is None:
+    uTimeout = INFINITE;
+  else:
+    uTimeout = long(nTimeout * 1000);
+  uWaitForSingleObjectResult = KERNEL32.WaitForSingleObject(hThread, uTimeout);
+  if uWaitForSingleObjectResult == WAIT_TIMEOUT:
+    return False; # Could not wait for it to die.
+  if uWaitForSingleObjectResult == WAIT_OBJECT_0:
+    return True; # Proces was terminated.
+  fThrowError(
+    "WaitForSingleObject(0x%08X, %d) = 0x%08X" % (hThread, guTimeout, uWaitForSingleObjectResult)
+  );
