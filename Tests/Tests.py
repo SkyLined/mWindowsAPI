4fe85d8763a52dab2984249d6123559985d8526f
SkyLined <github@skylined.nl>
2019-08-21 12:20:47 +0200

    Upgrade code that tests imported modules

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,36 +1,56 @@
-import os, re, sys, subprocess, threading, time;
+import json, os, sys;
+
+# Augment the search path to make the test subject a package and have access to its modules folder.
 sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
 sMainFolderPath = os.path.dirname(sTestsFolderPath);
 sParentFolderPath = os.path.dirname(sMainFolderPath);
 sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
 asOriginalSysPath = sys.path[:];
-sys.path = [sMainFolderPath, sParentFolderPath, sModulesFolderPath] + sys.path;
+sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
+# Load product details
+oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
+try:
+  dxProductDetails = json.load(oProductDetailsFile);
+finally:
+  oProductDetailsFile.close();
 # Save the list of names of loaded modules:
 asOriginalModuleNames = sys.modules.keys();
 
-from mWindowsAPI import *;
-from mWindowsSDK import *;
-oKernel32 = foLoadKernel32DLL();
-from mWindowsAPI import mDbgHelp;
+__import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
 
 # Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
 # loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
 # will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
-for sModuleName in sys.modules.keys():
-  assert (
-    sModuleName in asOriginalModuleNames # This was loaded before cBugId was loaded
-    or sModuleName.lstrip("_").split(".", 1)[0] in [
-      "mWindowsAPI", "mWindowsSDK", # This was loaded as part of the mWindowsAPI package
-      # These built-in modules are loaded by mWindowsAPI:
-      "base64", "binascii", "contextlib", "cStringIO", "ctypes", "encodings", "json", "nturl2path", "platform",
-      "socket", "ssl", "string", "strop", "struct", "textwrap", "urllib", "urlparse", "winreg",
-    ]
-  ), \
-      "Module %s was unexpectedly loaded outside of the mWindowsAPI package!" % sModuleName;
+asUnexpectedModules = list(set([
+  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
+  if not (
+    sModuleName in asOriginalModuleNames # This was loaded before
+    or sModuleName.lstrip("_").split(".", 1)[0] in (
+      [dxProductDetails["sProductName"]] +
+      dxProductDetails["asDependentOnProductNames"] +
+      [
+        # These built-in modules are expected:
+        "collections", "ctypes", "gc", "heapq", "itertools", "keyword",
+        "msvcrt", "platform", "string", "strop", "subprocess", "thread",
+        "threading", "time", "winreg"
+      ]
+    )
+  )
+]));
+assert len(asUnexpectedModules) == 0, \
+      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
+
+#Import the test subject
+from mWindowsAPI import *;
+from mWindowsSDK import *;
+oKernel32 = foLoadKernel32DLL();
+from mWindowsAPI import mDbgHelp;
+from mWindowsAPI.fThrowLastError import fThrowLastError;
+
 # Restore the search path
 sys.path = asOriginalSysPath;
 
-from mWindowsAPI.fThrowLastError import fThrowLastError;
+import re, subprocess, threading, time;
 
 if __name__ == "__main__":
   # Test system info
