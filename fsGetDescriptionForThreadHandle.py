9b94139e161a436027dad1bedf1ed399cff2ff2f
SkyLined <github@skylined.nl>
2019-10-01 12:33:28 +0200

    Take into account that feature may not be available

--- fsGetDescriptionForThreadHandle.py
+++ fsGetDescriptionForThreadHandle.py
@@ -5,6 +5,9 @@ from .fThrowLastError import fThrowLastError;
 def fsGetDescriptionForThreadHandle(ohThread):
   psDescription = PWSTR();
   oKernel32 = foLoadKernel32DLL();
+  if not oKernel32.GetThreadDescription:
+    # This functions is new to Windows 10, so it may not exist.
+    return None;
   if not oKernel32.GetThreadDescription(ohThread, psDescription.foCreatePointer()):
     fThrowLastError("GetThreadDescription(0x%X, ...)" % (ohThread.value,));
   uThreadDescriptionAddress = psDescription.value;
