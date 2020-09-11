1cd9f969722ab4ab7e596222668ab371f90772dc
SkyLined <github@skylined.nl>
2020-09-11 22:21:29 +0200

    Various improvements
    * bMustExist is more struct in that it checks the error code indicates the thread does not exist.
    * bMustGetAccess can be used to return an invalid handle if the request fails because access was denied, rather than throw an exception.
    * exception message is more descriptive

--- fohOpenForThreadIdAndDesiredAccess.py
+++ fohOpenForThreadIdAndDesiredAccess.py
@@ -1,15 +1,26 @@
 from mWindowsSDK import *;
 from .fbIsValidHandle import fbIsValidHandle;
+from .fsGetThreadAccessRightsFlagsDescription import fsGetThreadAccessRightsFlagsDescription;
 from .fThrowError import fThrowError;
 
-def fohOpenForThreadIdAndDesiredAccess(uThreadId, uDesiredAccess, bInheritHandle = False, bMustExist = True):
+def fohOpenForThreadIdAndDesiredAccess(uThreadId, uDesiredAccess, bInheritHandle = False, bMustExist = True, bMustGetAccess = True):
   oKernel32 = foLoadKernel32DLL();
   ohThread = oKernel32.OpenThread(DWORD(uDesiredAccess), BOOLEAN(bInheritHandle), DWORD(uThreadId));
   if not fbIsValidHandle(ohThread):
     # Save the last error because want to check if the thread exists, which may fail and modify it.
-    odwLastError = oKernel32.GetLastError();
-    if not bMustExist: # TODO! Checking if the thread exists is not implemented
+    udwLastError = oKernel32.GetLastError().value;
+    uhLastError = HRESULT_FROM_WIN32();
+    if not bMustGetAccess and uhLastError == ERROR_ACCESS_DENIED:
+      return HANDLE(INVALID_HANDLE_VALUE); # Cannot get the requested access to the thread; return an invalid handle
+    if not bMustExist and uhLastError == ERROR_INVALID_PARAMETER:
       return HANDLE(INVALID_HANDLE_VALUE); # No such thread exists; return an invalid handle
     # The thread exists; report an error:
-    fThrowError("OpenThread(0x%08X, FALSE, %d/0x%X)" % (uDesiredAccess, uThreadId, uThreadId), odwLastError.value);
+    fThrowError(
+      "OpenThread(dwDesiredAccess = 0x%08X (%s), bInheritHandle = FALSE, dwThreadId = 0x%X)" % (
+        uDesiredAccess,
+        fsGetThreadAccessRightsFlagsDescription(uDesiredAccess),
+        uThreadId,
+      ),
+      udwLastError
+    );
   return ohThread;
