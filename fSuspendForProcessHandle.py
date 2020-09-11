e187d5f1265c2ee01ec5afdfc8102fb7900af8af
SkyLined <github@skylined.nl>
2020-09-11 22:08:42 +0200

    Add code to get oNTDLL to avoid crash

--- fSuspendForProcessHandle.py
+++ fSuspendForProcessHandle.py
@@ -2,6 +2,7 @@ from mWindowsSDK import *;
 from .fThrowError import fThrowError;
 
 def fSuspendForProcessHandle(ohProcess):
+  oNTDLL = foLoadNTDLL();
   oNTStatus = oNTDLL.NtSuspendProcess(ohProcess); # NOT RELIABLE!
   if NT_ERROR(oNTStatus):
     fThrowError("NtSuspendProcess(0x%08X)" % (ohProcess.value,), oNTStatus.value);
