ff12c3353ce27b7d776cc49e7e5a9f23f26ac8c2
SkyLined <github@skylined.nl>
2020-09-11 22:04:36 +0200

    fSuspendForThreadHandle -> fbSuspendForThreadHandle
    The function now returns True if the thread was running before the call but was suspended after the call. It will return False if the thread was already suspended. Note that suspend calls are counted, so 2 calls to suspend require 2 calls to resume.

--- fbSuspendForThreadHandle.py
+++ fbSuspendForThreadHandle.py
@@ -1,7 +1,9 @@
 from mWindowsSDK import *;
 from .fThrowLastError import fThrowLastError;
 
-def fSuspendForThreadHandle(ohThread):
+def fbSuspendForThreadHandle(ohThread):
   oKernel32 = foLoadKernel32DLL();
-  if oKernel32.SuspendThread(ohThread) == -1:
+  odwSuspendCount = oKernel32.SuspendThread(ohThread);
+  if odwSuspendCount.value == -1:
     fThrowLastError("SuspendThread(0x%08X)" % (ohThread.value,));
+  return odwSuspendCount.value == 0;
