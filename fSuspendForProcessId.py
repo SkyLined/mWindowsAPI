749c2fde869bd62c20f578865cf94172045fd239
SkyLined <github@skylined.nl>
2020-09-11 22:02:59 +0200

    fSuspendForProcessId is now a wrapper for fSuspendForProcessHandle

--- fSuspendForProcessId.py
+++ fSuspendForProcessId.py
@@ -1,6 +1,6 @@
 from mWindowsSDK import *;
 from .fohOpenForProcessIdAndDesiredAccess import fohOpenForProcessIdAndDesiredAccess;
-from .fThrowError import fThrowError;
+from .fSuspendForProcessHandle import fSuspendForProcessHandle;
 from .fThrowLastError import fThrowLastError;
 
 def fSuspendForProcessId(uProcessId):
@@ -9,9 +9,7 @@ def fSuspendForProcessId(uProcessId):
   ohProcess = fohOpenForProcessIdAndDesiredAccess(uProcessId, THREAD_SUSPEND_RESUME);
   bSuccess = False;
   try:
-    oNTStatus = oNTDLL.NtSuspendProcess(ohProcess); # NOT RELIABLE!
-    if NT_ERROR(oNTStatus):
-      fThrowError("NtSuspendProcess(0x%08X)" % (ohProcess.value,), oNTStatus.value);
+    fSuspendForProcessHandle(ohProcess);
     bSuccess = True;
   finally:
     # Only throw an exception if one isn't already being thrown:
