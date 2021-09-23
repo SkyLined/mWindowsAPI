2b24ec00ddab2647691a70f4d7758a6fba695fbd
SkyLined <github@skylined.nl>
2021-09-23 14:58:37 +0200

    Use updated APIs for dependencies.

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -8,6 +8,7 @@ except ModuleNotFoundError as oException:
     raise;
   m0DebugOutput = None;
 
+guExitCodeInternalError = 1; # Use standard value;
 try:
   try:
     from mConsole import oConsole;
@@ -28,7 +29,6 @@ try:
         print(sOutput);
         sys.stdout.flush();
         oConsoleLock.release();
-      fPrint = fOutput;
       @staticmethod
       def fStatus(*txArguments, **dxArguments):
         pass;
@@ -136,5 +136,5 @@ try:
   
 except Exception as oException:
   if m0DebugOutput:
-    m0DebugOutput.fTerminateWithException(oException, bShowStacksForAllThread = True);
+    m0DebugOutput.fTerminateWithException(oException, guExitCodeInternalError, bShowStacksForAllThread = True);
   raise;
