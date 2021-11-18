803b9ede26ab4b19350309ebc985d86bfb39efbc
SkyLined <github@skylined.nl>
2021-11-18 15:31:20 +0100

    Fix argument type in call to Kernel32 IsWow64Process

--- fsGetISAForProcessHandle.py
+++ fsGetISAForProcessHandle.py
@@ -7,8 +7,8 @@ if oSystemInfo.sOSISA == "x86":
     return "x86"; # Not other option
 else:
   def fsGetISAForProcessHandle(ohProcess):
-    obIsWow64Process = BOOLEAN();
     oKernel32 = foLoadKernel32DLL();
+    obIsWow64Process = BOOL();
     opobIsWow64Process = obIsWow64Process.foCreatePointer()
     if not oKernel32.IsWow64Process(ohProcess, opobIsWow64Process):
       fThrowLastError("IsWow64Process(%s, %s)" % (repr(ohProcess), repr(opobIsWow64Process)));
