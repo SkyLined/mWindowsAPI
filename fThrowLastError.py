071738a936e9a2a0b4b89ad82f8afb2d1610be3d
SkyLined <cojmpjum_github.com@sxd.be>
2022-08-27 01:00:02 +0200

    Hide `fThrowLastError` in call stack

--- fThrowLastError.py
+++ fThrowLastError.py
@@ -2,5 +2,6 @@ from mWindowsSDK.mKernel32 import oKernel32DLL;
 from .fThrowWin32Error import fThrowWin32Error;
 
 def fThrowLastError(sFailedOperation):
+  mDebugOutput_HideInCallStack = True; # Hide this helper function in the call stack.
   odwLastError = oKernel32DLL.GetLastError();
   return fThrowWin32Error(sFailedOperation, odwLastError.fuGetValue());
